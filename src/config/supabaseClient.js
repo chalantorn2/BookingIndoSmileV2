6ec6fc65076e4bc1b9cba3e3999d886cd2e0ae16
chalantorn2 <chalantorn2@gmail.com>
2025-05-24 15:29:24 +0700

    update env

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://sixwsodfumroethflcst.supabase.co"; // แก้เป็นของคุณ
+const SUPABASE_URL = "https://itehwlzixbylnmxjxcmv.supabase.co"; // แก้เป็นของคุณ
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpeHdzb2RmdW1yb2V0aGZsY3N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE1ODA1MTYsImV4cCI6MjA1NzE1NjUxNn0.UmeXSf-aKFrOk2HoXHlt3l8OAlwsTlhPIjsm08dVRHQ"; // แก้เป็นของคุณ
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0ZWh3bHppeGJ5bG5teGp4Y212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwNzI0NjMsImV4cCI6MjA2MzY0ODQ2M30.y4qOApQEknv_e9kozwPznOqCqAoxqFdsxaUbWsI7Xts"; // แก้เป็นของคุณ
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
